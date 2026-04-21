d101b71376c15b8b70b4ee406e889079d5e7c68c
Sator-Apolo <portaldigitalofficiall@gmail.com>
2026-04-21 20:54:09 -0300

    Add OneSignal SDK worker script for notifications
    Obrero

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,14 @@
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignal.SDK.js" defer></script>
+<script>
+  window.OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "e29ae115-e1d1-4e0d-a062-491d38b49cab",
+      safari_web_id: "web.onesignal.auto.1764835a-8b8b-4a5e-b83c-62c7237a3465", // Opcional
+      notifyButton: {
+        enable: true, // Esto activa el "campanito" flotante para que sea más fácil suscribirse
+      },
+      allowLocalhostAsSecureOrigin: true,
+    });
+  });
+</script>
